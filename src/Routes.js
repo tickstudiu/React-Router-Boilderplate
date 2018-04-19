f70f547859dea224f2973c4ea11f9d3ff6720612
PanupongDeve <5635512110@psu.ac.th>
2018-04-20 00:00:33 +0700

    edit route

--- src/Routes.js
+++ src/Routes.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import indexRoutes from './imports/routes';
+import indexRoutes from "./imports/routes";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-        {
-          indexRoutes.map((route) => {
-            return (
-              <Route 
-                exact path={route.path}
-                key={route.name}
-                component={route.component}
-              />
-            );
-          })
-        }
+          {indexRoutes.map(route => (
+            <Route
+              exact
+              path={route.path}
+              key={route.name}
+              component={route.component}
+            />
+          ))}
         </Switch>
       </Router>
     );

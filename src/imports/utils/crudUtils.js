e23cad2754d5ba461ca974e64a937a0058fdc881
PanupongDeve <5635512110@psu.ac.th>
2018-04-20 12:57:18 +0700

    update utils

---
+++ src/imports/utils/crudUtils.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+export const getApi = (url) => {
+
+}
+
+export const getApiById = (id, url) => {
+
+}
+
+export const postApi = (url, data) => {
+
+}
+
+export const patchApi = (url, data) => {
+
+}
+
+export const deleteApi = (url) => {
+    
+}

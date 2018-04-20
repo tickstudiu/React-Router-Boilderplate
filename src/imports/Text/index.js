0c23857abee9f00760d96218f816306dc1c7af16
PanupongDeve <5635512110@psu.ac.th>
2018-04-20 12:58:34 +0700

    update

--- src/imports/Text/index.js
+++ src/imports/Text/index.js
@@ -9,3 +9,19 @@ export const Page2Text = {
     redirectName: 'Go to Page1',
     redirect: '/'
 }
+
+
+export const LoginText = {
+    th: {
+        header: 'ลงชื่อเข้าใช้งาน',
+        submit: 'เข้าสู่ระบบ',
+        forgot: 'ลืมผู้ใช้งาน , อีเมลล์ หรือรหัสผ่าน ',
+        forgotClick: 'คลิกที่นี่'
+    },
+    en: {
+        header: 'LOGIN TO ACCOUNT',
+        submit: 'Submit',
+        forgot: 'forgot user, email or password, ',
+        forgotClick: 'Click here'
+    }
+}
